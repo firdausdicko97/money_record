091dea5c464b4147913a5c925c02f1f901343e88
firdausdicko97 <firdausdicko@gmail.com>
2023-06-15 15:18:01 +0700

    register UI

--- lib/data/source/source_user.dart
+++ lib/data/source/source_user.dart
@@ -1,3 +1,4 @@
+import 'package:d_info/d_info.dart';
 import 'package:money_record/config/api.dart';
 import 'package:money_record/config/app_request.dart';
 import 'package:money_record/config/session.dart';
@@ -14,6 +15,32 @@ class SourceUser {
     if (responseBody['success']) {
       var mapUser = responseBody['data'];
       Session.saveUser(User.fromJson(mapUser));
+    }
+    return responseBody['success'];
+  }
+
+  static Future<bool> register(
+      String name, String email, String password) async {
+    String url = '${api.user}/register.php';
+    Map? responseBody = await AppRequest.post(url, {
+      'name': name,
+      'email': email,
+      'password': password,
+      'created_at': DateTime.now().toIso8601String(),
+      'updated_at': DateTime.now().toIso8601String(),
+    });
+
+    if (responseBody == null) return false;
+
+    if (responseBody['success']) {
+      DInfo.dialogSuccess('Berhasil register');
+      DInfo.closeDialog();
+    } else {
+      if (responseBody['message'] == 'email') {
+        DInfo.dialogError('Email sudah terdaftar');
+      } else {
+        DInfo.dialogError('Gagal register');
+      }
     }
     return responseBody['success'];
   }
--- lib/presentation/page/home_page.dart
+++ lib/presentation/page/home_page.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:get/get.dart';
+import 'package:money_record/config/session.dart';
+import 'package:money_record/presentation/page/auth/login_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Login Page')),
+    return Scaffold(
+      body: Center(
+        child: Row(children: [
+          Text('Home Page'),
+          IconButton(
+            onPressed: () {
+              Session.clearUser();
+              Get.off(() => const LoginPage());
+            },
+            icon: Icon(Icons.logout),
+          )
+        ]),
+      ),
     );
   }
 }
